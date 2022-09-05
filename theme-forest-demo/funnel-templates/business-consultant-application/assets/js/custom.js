3e9fb6b866cae950e2a91265019110ed45458f36
baamelyoussef <imbaamelyoussef@gmail.com>
2022-09-05 15:39:02 +0100

    init

---
+++ theme-forest-demo/funnel-templates/business-consultant-application/assets/js/custom.js
@@ -0,0 +1,156 @@
+	/*=========================================
+		Preload Spinner
+	=========================================*/
+	$(window).on('load', function(){
+		setTimeout(removeLoader, 300);
+	});
+	function removeLoader(){
+		$( ".preloadSpinner" ).fadeOut(200, function() {
+		$( ".preloadSpinner" ).remove();  
+		});  
+	}
+
+	$(document).ready(function($) {
+    /*=========================================
+		05. Registration Form
+	=========================================*/
+	if($("#free-consultation-form").length) {
+		$('#free-consultation-form').validate({
+			errorPlacement: function(error, element) {
+				return true;
+			},
+			rules: {
+				full_name: {
+					required: true,
+					minlength: 3
+				},
+				email: {
+					required: true,
+					email: true
+				},
+			},
+			submitHandler: function(form) {
+				var formData = $('#free-consultation-form').serialize();
+				$.ajax({
+					type: 'POST',
+					url: 'assets/php/popup-form.php',
+					dataType: "json",
+					data: formData,
+					success: function(data) {
+						if(data.success) {
+							$('.form-status').addClass('alert alert-success');
+							$('.form-status').text('Your Message Has been Sent Successfully');
+							form.submit();
+							$('.form-status').slideDown().delay(3000).slideUp();
+							$("#free-consultation-form").trigger("reset");
+							window.location.href = 'oto.html';
+						} else {
+							$('.form-status').addClass('alert alert-danger');
+							$('.form-status').text('Error Occurred, Please Try Again');
+							$('.form-status').slideDown().delay(3000).slideUp();
+						}
+					},
+					error: function(xhr, status, error) {
+						$('.form-status').addClass('alert alert-danger');
+						$('.form-status').text('Something Went Wrong');
+						$('.form-status').slideDown().delay(3000).slideUp();
+					}
+				});
+			}
+		});
+	}
+
+	/*=========================================
+		Order Form
+	=========================================*/
+
+	if ($("#order-form").length) {
+		$("#order-form").validate({
+			errorPlacement: function(error,element) {
+                return true;
+            },
+			rules: {
+				first_name: {
+					required: true,
+					minlength: 3
+				},
+				last_name: {
+					required: true,
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				phone: {
+					required: true,
+				},
+				billing_address: {
+					required: true,
+				},
+				city: {
+					required: true,
+				},
+				state: {
+					required: true,
+				},
+				
+				country: {
+					required: true,
+				},
+				zip: {
+					required: true,
+				},
+			},
+			submitHandler: function(form) {
+				var formData = $('#order-form').serialize();
+				$.ajax({
+					type: 'POST',
+					url: 'assets/php/order-form.php',
+					dataType: "json",
+					data: formData,
+					success: function (data) {
+						if (data.success) {
+							$('.form-status').addClass('alert alert-success');
+							$('.form-status').text('Your Message Has been Sent Successfully');
+							$('.form-status').slideDown().delay(3000).slideUp();
+							$("#order-form").trigger("reset");
+							form.submit();
+						} else {
+							$('.form-status').addClass('alert alert-danger');
+							$('.form-status').text('Error Occurred, Please Try Again');
+							$('.form-status').slideDown().delay(3000).slideUp();
+						}
+					},
+					error: function (xhr, status, error) {
+						$('.form-status').addClass('alert alert-danger');
+						$('.form-status').text('Something Went Wrong');
+						$('.form-status').slideDown().delay(3000).slideUp();
+					}
+				});
+			}
+		});
+	}
+	
+	/*=========================================
+		Back To Top
+	=========================================*/
+	$(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('.backtotop').fadeIn(100);
+        } else { 
+            $('.backtotop').fadeOut(100); 
+        } 
+    }); 
+    $('.backtotop').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 100); 
+        return false; 
+    }); 
+
+	/*=========================================
+		Dynamic Date
+	=========================================*/
+		// Jquery Code below for footer copyright Year
+		$('#copyright_year').html(new Date().getFullYear());
+
+
+});
